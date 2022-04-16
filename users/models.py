3388a2cd0e165a7bcba9618974e067e8736a7ad6
do-not-do-that <rl7968@naver.com>
2022-04-17 02:08:34 +0900

    feat: 회원가입,로그인 기능 구현완료

--- users/models.py
+++ users/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    # 일반 user 생성
+    def create_user(self, email, nickname, name, password=None):
+        if not email:
+            raise ValueError('must have user email')
+        if not nickname:
+            raise ValueError('must have user nickname')
+        if not name:
+            raise ValueError('must have user name')
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname,
+            name=name
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(default='', max_length=100, null=False, blank=False, unique=True)
+    nickname = models.CharField(default='', max_length=100, null=False, blank=False, unique=True)
+    name = models.CharField(default='', max_length=100, null=False, blank=False)
+
+    # User 모델의 필수 field
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    # 헬퍼 클래스 사용
+    objects = UserManager()
+
+    USERNAME_FIELD = 'nickname'
+
+
+    REQUIRED_FIELDS = ['email', 'name']
+
+    def __str__(self):
+        return self.nickname
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .serializers import UserSerializer
+from .models import User
+from rest_framework import generics
 
-# Create your views here.
+# 회원가입
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
